14d67f17185e5ef59229eddfafcffd987e369eec
Virendra patel <patelvirendra62@gmail.com>
2020-10-19 00:40:44 +0530

    base.html

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from app.views import home , login , signup
 
 
 urlpatterns = [
-   path('' , home ), 
-   path('login' ,login ), 
-   path('signup' , signup ), 
+   path('' , home , name='home' ), 
+   path('login/' ,login  , name='login'), 
+   path('signup/' , signup ), 
 ]
--- app/views.py
+++ app/views.py
@@ -1,12 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate , login as loginUser
+from django.contrib.auth.forms import UserCreationForm , AuthenticationForm
 # Create your views here.
 
 def home(request):
     return render(request , 'index.html')
 
 def login(request):
-    return render(request , 'login.html')
+    if request.method == 'GET':
+        form1 = AuthenticationForm()
+        context = {
+            "form" : form1
+        }
+        return render(request , 'login.html' , context=context )
+    else:
+        form = AuthenticationForm(data=request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username = username , password = password)
+            if user is not None:
+                loginUser(request , user)
+                return redirect('home')
+        else:
+            context = {
+                "form" : form
+            }
+            return render(request , 'login.html' , context=context )
+
 
 def signup(request):
-    return render(request , 'signup.html')
+    if request.method == 'GET':
+        form = UserCreationForm()
+        context = {
+            "form" : form
+        }
+        return render(request , 'signup.html' , context=context)
+    else:
+        print(request.POST)
+        form = UserCreationForm(request.POST)  
+        context = {
+            "form" : form
+        }
+        if form.is_valid():
+            user = form.save()
+            print(user)
+            if user is not None:
+                return redirect('login')
+        else:
+            return render(request , 'signup.html' , context=context)
