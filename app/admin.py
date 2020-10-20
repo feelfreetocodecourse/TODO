0df105adfc5a48310b04daa09c0a38db7c4f52fc
Virendra patel <patelvirendra62@gmail.com>
2020-10-20 12:32:56 +0530

    model

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from app.models import TODO
 # Register your models here.
+admin.site.register(TODO)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+#  title 
+#  status
+#  date - current 
+#  user 
+#  priority
+
+
+class TODO(models.Model):
+    status_choices = [
+    ('C', 'COMPLETED'),
+    ('P', 'PENDING'),
+    ]
+    priority_choices = [
+    ('1', '1️⃣'),
+    ('2', '2️⃣'),
+    ('3', '3️⃣'),
+    ('4', '4️⃣'),
+    ('5', '5️⃣'),
+    ('6', '6️⃣'),
+    ('7', '7️⃣'),
+    ('8', '8️⃣'),
+    ('9', '9️⃣'),
+    ('10', '🔟'),
+    ]
+    title = models.CharField(max_length=50)
+    status = models.CharField(max_length=2 , choices=status_choices)
+    user  = models.ForeignKey(User  , on_delete= models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    priority = models.CharField(max_length=2 , choices=priority_choices)
